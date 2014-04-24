5f17d5495fb4ccc6f2c37f138e0faed342998905
Akshay <aktwo@princeton.edu>
2014-04-24 13:12:10 -0400

    Removed authenticity token for json files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_filter :verified_request?
+  def verified_request?
+    if request.content_type == "application/json"
+      true
+    else
+      super()
+    end
+  end
+
   protect_from_forgery with: :exception
+
 end
